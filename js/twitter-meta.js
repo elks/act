e474bcce9335b8880886bec4d762b61e5ea8199e
elks <git@soderstrome.com>
2015-07-17 17:45:00 -0400

    JS Set Twitter Tags

--- js/twitter-meta.js
+++ js/twitter-meta.js
@@ -1,1 +1,1 @@
-document.getElementsByTagName('head')[0].appendChild("<meta name='twitter:site' content='@ACTforAmerica'>");
+document.getElementsByTagName('head')[0].appendChild("<meta name='twitter:site' content='@ACTforAmerica'><meta name='twitter:card' content-'summary'");
