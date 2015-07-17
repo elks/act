d312e9641cc8ea53c5b81e154601e66adf7c5554
elks <git@soderstrome.com>
2015-07-17 17:56:33 -0400

    Javascript

--- js/twitter-meta.js
+++ js/twitter-meta.js
@@ -1,1 +1,4 @@
+function twittermeta() {
 document.getElementsByTagName('head')[0].appendChild("<meta name='twitter:site' content='@ACTforAmerica'><meta name='twitter:card' content-'summary'");
+}
+window.onload = twittermeta()
